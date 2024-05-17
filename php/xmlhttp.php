77236d0273657dec7aa0bb961aba283148ba6369
casa-apps <adnan@casaapps.com>
2024-05-17 14:54:30 +0500

    17/05/2024

--- php/xmlhttp.php
+++ php/xmlhttp.php
@@ -4,19 +4,65 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style>
+        table{
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        table th, table td{
+            padding: 5px 10px;
+            border: 1px solid grey;
+            border-collapse: collapse;
+        }
+    </style>
 </head>
 <body>
     
+
+    <table>
+        <thead>
+            <tr>
+                <th>Currency</th>
+                <th>Rate</th>
+                <th>Description</th>
+            </tr>
+        </thead>
+        <tbody id="demo"></tbody>
+    </table>
 
 
     <script>
         const xhttp = new XMLHttpRequest();
         xhttp.onload = function() {
-            console.log( JSON.parse(this.responseText) );
+            
+            let data = JSON.parse(this.responseText);
+
+            console.log(data)
+            let html = '';
+
+            for( key in data){
+                console.log(data[key]);
+                if(key == 'bpi'){
+                    for(currency in data[key]){
+                        html += `<tr>
+                        <td>${currency}</td>
+                        <td>${data[key][currency].rate}</td>
+                        <td>${data[key][currency].description}</td>
+                        </tr>`
+                    }
+                    document.getElementById("demo").innerHTML = html;
+                }
+            }
             // document.getElementById("demo").innerHTML = this.responseText;
         }
-        xhttp.open("GET", "https://ecom.test/php/get_students.php", true);
+        // xhttp.open("GET", "https://ecom.test/php/get_students.php", true);
+        xhttp.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json", true);
         xhttp.send();
+
+
+        // window.location = "http://google.com";
     </script>
 
 </body>
